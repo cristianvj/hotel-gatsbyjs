58efa764235a3aa3818e8d7e0f1cb602d1661ad8
Cristian Villota <cristiandavid@Cristians-MacBook-Air.local>
2020-10-14 11:39:24 -0500

    Descomentado linea de error

--- src/components/ContenIdoInicio.jsx
+++ src/components/ContenIdoInicio.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const  ContenidoInicio  = () => {
-  // const informacion = useStaticQuery(graphql`
-  //   query {
-  //       allDatoCmsPagina(filter: { slug: { eq: "inicio" } }) {
-  //             nodes {
-  //             titulo
-  //             contenido
-  //             imagen {
-  //               fluid {
-  //                  ...GatsbyDatoCmsFluid
-  //               }
-  //             }
-  //           }
-  //       }
-  //     }
-  //   `);
+  const informacion = useStaticQuery(graphql`
+    query {
+        allDatoCmsPagina(filter: { slug: { eq: "inicio" } }) {
+              nodes {
+              titulo
+              contenido
+              imagen {
+                fluid {
+                   ...GatsbyDatoCmsFluid
+                }
+              }
+            }
+        }
+      }
+    `);
 
-    //  console.log(informacion.allDatoCmsPagina.nodes[0]);
+    console.log(informacion.allDatoCmsPagina.nodes[0]);
 
   //console.log(informacion);
   return (
