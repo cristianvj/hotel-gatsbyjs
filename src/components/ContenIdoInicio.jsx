42573e032481f85d56195ab92022fc73680e59f0
Cristian Villota <cristiandavid@Cristians-MacBook-Air.local>
2020-10-13 22:55:34 -0500

    Error en la consulta de GraphQl

---
+++ src/components/ContenIdoInicio.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+
+const  ContenidoInicio  = () => {
+  // const informacion = useStaticQuery(graphql`
+  //   query {
+  //       allDatoCmsPagina(filter: { slug: { eq: "inicio" } }) {
+  //             nodes {
+  //             titulo
+  //             contenido
+  //             imagen {
+  //               fluid {
+  //                  ...GatsbyDatoCmsFluid
+  //               }
+  //             }
+  //           }
+  //       }
+  //     }
+  //   `);
+
+    //  console.log(informacion.allDatoCmsPagina.nodes[0]);
+
+  //console.log(informacion);
+  return (
+    <>
+      <h2>Titulo de Página</h2>
+    </>
+  );
+}
+
+export default ContenidoInicio;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import {Global, css} from '@emotion/core';
+import Header from './Header';
+
+const Layout = props => {
+  return (
+    <>
+      <Global
+        styles={css`
+        html{
+          font-size: 62.5%;
+        }
+          body{
+            font-size: 16px;
+            font-size: 1.6rem;
+            line-height: 1.5;
+            font-family: 'PT Sans', sans-serif;
+          }
+          h1,h2,h3{
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1,h2{
+            font-family: 'Roboto', serif;
+          }
+          h3{
+            font-family: 'PT Sans', sans-serif;
+          }
+          ul{
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
+      <Helmet>
+        <title>Gatsby Hotel</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+      </Helmet>
+      <Header/>
+      {props.children}
+    </>
+  );
+}
+
+export default Layout;
